2051ecd886e678933ffecc36af521566e01f59dc
kwpark <welcomepkw@gmail.com>
2018-03-12 15:01:31 +0900

    modify egov files and config

--- src/main/java/com/sample/common/egov/EgovTextPaginationRenderer.java
+++ src/main/java/com/sample/common/egov/EgovTextPaginationRenderer.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.sample.common;
+package com.sample.common.egov;
 
 import egovframework.rte.ptl.mvc.tags.ui.pagination.AbstractPaginationRenderer;
 
